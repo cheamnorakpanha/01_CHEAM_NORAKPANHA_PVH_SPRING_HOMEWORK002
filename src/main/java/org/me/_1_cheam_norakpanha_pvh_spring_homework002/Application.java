37641415b32bd511dbfc71df8972c3ac96df2f87
Cheam Norakpanha <cheam.panha168@gmail.com>
2026-03-17 10:52:43 +0700

    first commit

---
+++ src/main/java/org/me/_1_cheam_norakpanha_pvh_spring_homework002/Application.java
@@ -0,0 +1,13 @@
+package org.me._1_cheam_norakpanha_pvh_spring_homework002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/me/_1_cheam_norakpanha_pvh_spring_homework002/Application.java
@@ -0,0 +1,13 @@
+package org.me._1_cheam_norakpanha_pvh_spring_homework002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
